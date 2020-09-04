d92946a7e65318256d692bc2344b17d801f1afce
Soumya121099 <70697901+Soumya121099@users.noreply.github.com>
2020-09-04 17:13:02 +0530

    Add files via upload

---
+++ For.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Loops
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int index = 0; index < 10; index++)
+			{
+				Console.WriteLine($"Hello World! The index is {index}");
+			}
+		}
+	}
+}
