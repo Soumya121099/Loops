93d0126bafdf518936aee2132f6e8c8e3417bf26
Soumya121099 <70697901+Soumya121099@users.noreply.github.com>
2020-09-04 17:08:49 +0530

    Add files via upload

---
+++ DoWhile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Loops
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int counter = 0;
+			do
+			{
+				Console.WriteLine($"Hello World! The counter is {counter}");
+				counter++;
+			} while (counter < 10);
+		}
+	}
+}
