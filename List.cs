f6bed23db971e58df16aaae79c25c26465e9b44d
Soumya121099 <70697901+Soumya121099@users.noreply.github.com>
2020-09-04 17:36:49 +0530

    Add files via upload

--- List.cs
+++ List.cs
@@ -1,16 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-namespace Lists
+
+namespace CList
 {
 	class Program
 	{
-		static void Main(string[] args)
-		{
-                var names = new List<string> { "<name>", "Ana", "Felipe" };
-                foreach (var name in names)
-                {
-                    Console.WriteLine($"Hello {name.ToUpper()}!");
-                }
+        static void Main(string[] args)
+        {
+            var emplist = new List<String>();
+
+            emplist.Add("Soumya");
+            emplist.Add("Mahak");
+            emplist.Add("Gautam");
+            emplist.Add("Tom");
+            emplist.Add("Vinitha");
+            emplist.Add("Sravani");
+
+            Console.WriteLine("Employees Present in List:\n");
+
+            int pos = 0;
+
+            foreach (string name in emplist)
+            {
+                Console.Write("At Position {0}: ", pos);
+                Console.WriteLine(name.ToUpper());
+                pos++;
             }
+
+            Console.WriteLine(" ");
+ 
+            Console.WriteLine("Removing the employee at index 3\n");
+ 
+            emplist.RemoveAt(3);
+
+            int pos1 = 0;
+
+            foreach (string n in emplist)
+            {
+                Console.Write("At Position {0}: ", pos1);
+                Console.WriteLine(n.ToLower());
+                pos1++;
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("The sorted list of employee names:\n");
+            emplist.Sort();
+            int pos2=0;
+            foreach (var names in emplist)
+            {
+                Console.Write("At Position {0}: ", pos2);
+                Console.WriteLine(names);
+                pos2++;
+            }
+            Console.WriteLine(" ");
+            var index = emplist.IndexOf("Vinitha");
+            Console.WriteLine($"Vinitha is found at {index}");
         }
-    }
+	}
+}
